96db392a599b789e07cf0f9df564343e6788241c
Igor Slobodskov <igor.slobodskov@phystech.edu>
2018-04-26 14:32:27 +0300

    add simple utility class: 2d array

---
+++ core/src/com/github/kright/utils/Array2d.scala
@@ -0,0 +1,24 @@
+package com.github.kright.utils
+
+import scala.collection.mutable.ArrayBuffer
+
+class Array2d[T](val width: Int, val height: Int) {
+
+  private var array = new ArrayBuffer[T](width * height)
+
+  @inline
+  private def check(x: Int, y: Int): Unit = {
+    assert(x >= 0 && x < width && y >= 0 && y < height,
+      s"Array2d(width=$width, height=$height) InvalidIndices($x, $y)")
+  }
+
+  @inline
+  private def pos(x: Int, y: Int): Int = {
+    check(x, y)
+    x + width * y
+  }
+
+  def apply(x: Int, y: Int): T = array(pos(x, y))
+
+  def update(x: Int, y: Int, value: T): Unit = array(pos(x, y)) = value
+}
