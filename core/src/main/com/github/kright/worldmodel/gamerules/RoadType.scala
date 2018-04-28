dccdeaad7176e3c49c511716118d00bdbd57b9e9
Igor Slobodskov <igor.slobodskov@phystech.edu>
2018-04-28 18:40:26 +0300

    change road descriptions:
    * NoRoad: ok
    * Road will have parameter: speed multiplier
    * RailRoad will have parameter: max distance multiplier

--- core/src/main/com/github/kright/worldmodel/gamerules/RoadType.scala
+++ core/src/main/com/github/kright/worldmodel/gamerules/RoadType.scala
@@ -7,6 +7,13 @@ sealed trait RoadType
 
 case object NoRoad extends RoadType
 
-case object Road extends RoadType
+/**
+  * @param movesMultiplier how much faster units move on roads
+  */
+case class Road(movesMultiplier: Int) extends RoadType
 
-case object Railroad extends RoadType
+/**
+  * @param maxDistance max distance for moving on railroad
+  *                    distance doesn't depends on moves count of unit
+  */
+case class Railroad(maxDistance: Int) extends RoadType
