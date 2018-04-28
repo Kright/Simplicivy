23c81e4491ea2e6f118a0cbc6624185d130fcb79
Igor Slobodskov <igor.slobodskov@phystech.edu>
2018-04-28 23:39:23 +0300

    add comment for GameRules about consistency of state

--- core/src/main/com/github/kright/worldmodel/gamerules/GameRules.scala
+++ core/src/main/com/github/kright/worldmodel/gamerules/GameRules.scala
@@ -6,8 +6,12 @@ import com.github.kright.utils.ObjectOwner
   * Created by Igor Slobodskov on 27 April 2018
   *
   * effectively persistent structure about game rules
-  * technologies tree, city buildings, unit types.
+  * technologies tree, city buildings, unit types and what needed to play a game
   *
+  * Some information may be redundant:
+  * for example, Resource contents links to several terrain types (on which them may be placed)
+  * and information has to be consistent.
+  * resources collection must contain all possible values! And about others: too
   */
 trait GameRules {
 
