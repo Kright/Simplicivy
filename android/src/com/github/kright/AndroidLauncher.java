16716ec355f6b97fa3d173f652ba62c153286bfc
Igor Slobodskov <igor.slobodskov@phystech.edu>
2018-05-05 17:32:35 +0300

    change config for android: enable immersive mode, disable unused options

--- android/src/com/github/kright/AndroidLauncher.java
+++ android/src/com/github/kright/AndroidLauncher.java
@@ -6,10 +6,18 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Main(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+        config.useCompass = false;
+        config.useAccelerometer = false;
+        config.r = config.g = config.b = config.a = 8;
+
+        config.useImmersiveMode = true;
+        config.hideStatusBar = true;
+
+        initialize(new Main(), config);
+    }
 }
